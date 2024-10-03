a7aa7ce3b484d5459e60fac52e4dec36f9abe1c0
felipeniano <148362411+felipeniano@users.noreply.github.com>
2024-10-03 14:15:59 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,22 @@
+criaCartao (
+    'Vôlei',
+    'Curiosiade sobre vôlei ?',
+    'A primeira quadra de voleibol tinha cerca de 15 metros de comprimento por 7,60 metros de largura.'
+)
+criaCartao (
+    'Vôlei',
+   'O que o vôlei exige?',
+    'a competência de ter reflexos e direcionamento bem definidos.'
+)
+
+criaCartao (
+    'Vôlei',
+   'Qual foi a primeira bola usada no vôlei?',
+    'A primeira bola usada no voleibol foi uma câmara de bola de Basquetebol, mas era muito pesada.'
+)
+criaCartao (
+    'Vôlei',
+   'Quando o vôlei chegou ao brasil?',
+    'No Brasil, o primeiro registro de uma partida de vôlei foi no ano de 1915.'
+)
+
